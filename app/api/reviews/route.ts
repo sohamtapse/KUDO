a993f3d5c286df3777e1037bc4b206b198e30856
sohamtapse <tapsesoham0@gmail.com>
2025-11-18 00:08:15 +0530

    did not know why i did it

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import Review from "@/models/Review";
+import "../../../db/db";
 
 export async function GET() {
   const reviews = await Review.find().sort({ createdAt: -1 });
